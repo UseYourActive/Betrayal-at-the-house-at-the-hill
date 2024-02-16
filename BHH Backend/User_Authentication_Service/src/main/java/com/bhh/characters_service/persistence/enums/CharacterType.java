a803e76d61371dfebec01288cd51f49e9610e401
BogomilDonkov <bogson223@gmail.com>
2024-02-16 23:43:43 +0200

    - update

--- BHH Backend/User_Authentication_Service/src/main/java/com/bhh/characters_service/persistence/enums/CharacterType.java
+++ BHH Backend/User_Authentication_Service/src/main/java/com/bhh/characters_service/persistence/enums/CharacterType.java
@@ -8,16 +8,23 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 public enum CharacterType {
-    ZOE_INGSTROM("Zoe Ingstrom", 8, "November 5th", List.of("nz"), 49, 3.9, 4, 3, 5 , 3);
-    //BRANDON_JASPERS("Brandon Jaspers", 12, "May 21st", List.of("Computers", "Camping", "Hocket"), 109, 5.1);
+    ZOE_INGSTROM("Zoe Ingstrom",  "November 5th", "Dolls, Music", 8,"49 lbs", "3'9\"", 4, 3, 5 , 3),
+    BRANDON_JASPERS("Brandon Jaspers",  "May 21st", "Computers, Campaign, Hockey",12, "109 lbs", "5'1\"",4,4,4,3),
+    ;
+
+    //region Plot Information
     private final String name;
-    private final Integer age;
     private final String birthday;
-    private final List<String> hobbies;
-    private final Integer weight;
-    private final Double height;
+    private final String hobbies; //Used for game plot, not really useful, so we leave it as String
+    private final Integer age;
+    private final String weight;
+    private final String height;
+    //endregion
+
+    //region Starting stats
     private final Integer speed;
     private final Integer might;
     private final Integer sanity;
     private final Integer knowledge;
+    //endregion
 }
